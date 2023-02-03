127dd02d122c927120d98db1635f5078dd754634
Jason Shaun DMello <jason.dmello21@gmail.com>
2023-02-04 00:24:59 +0530

    Fixed bugs in multiple codes, and completed some incomplete ones

--- bot_functions.c
+++ bot_functions.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int queue_size;
-int queue[]; //queue for storing the path
+/*This project uses flood fill algorithm Pseudocode is as follows - 
+    1. Move mouse to lowest cost node
+    2. If all neighbors are higher than or equal to current node, push node to queue.
+    3. increment current node by 1 and push acessible neighbors to queue
+    4. repeat step 3 until queue is empty
+*/
 
 void swap(int *x, int *y){
+    //Swaps values of two  numbers x and y. 
+    //Fucntion is complete
     int temp = *x;
     *x = *y;
     *y = temp;
@@ -12,23 +18,26 @@ void swap(int *x, int *y){
 
 int* minimum_cost(int **arena_map, int bot_pos[2]){
     /*
-        returns arnray with [0,1,2,3] as [l,s,r,b] in ascending order
+        returns array with [0,1,2,3] as [l,s,r,b] in ascending order
+        Function unverified
         to do: check for array out of bound
     */
 
+   //Getting values of neighbors
     int top    = arena_map[bot_pos[0] - 1][bot_pos[1]];
     int bottom = arena_map[bot_pos[0] + 1][bot_pos[1]];
     int left   = arena_map[bot_pos[0]][bot_pos[1] - 1];
     int right  = arena_map[bot_pos[0]][bot_pos[1] + 1];
-    int *return_value = calloc (4, sizeof(int));
-    int temp_arr[4] = {left, top, right, bottom};
+
+    int *return_value = calloc (4, sizeof(int)); //array to be returned
+    int temp_arr[4] = {left, top, right, bottom}; //array to be sorted
     int smallest =0, temp = 0;
     
     for (int i = 0; i<4; i++){
-        return_value[i] =i;
+        return_value[i] =i; //initializing return array to [0,1,2,3]
     }
 
-    //Sorting array
+    //Sorting array (selection sort)
     for (int i  =0 ; i<4; i++){
         smallest = i;
         for (int j = i +1;j<4; j++){
@@ -45,42 +54,61 @@ int* minimum_cost(int **arena_map, int bot_pos[2]){
 
 int* detect_wall(){
     /*Siya needs to update
-    returns an array [l, s, r, b] with 1 if wall is detected and 0 if not*/
+    returns an array [l, s, r, b] with 1 if wall is detected and 0 if not
+    Function unverified*/
 }
 
 void rearrange_map(int ** arena_map, int base_pos[2]){
-    /*Not Complete*/
-    queue_push(base_pos[0], base_pos[1]);
+    //Changes value of map node cost in case the current node has a strictly lower cost than all of its accessible neighbors. Function unverified
+
+    queue_push(base_pos[0], base_pos[1]); //pushing base node to queue
     int *poped;
     int min_access;
+    int *wall_array;
+
     while (!queue_empty()){
         poped = queue_pop();
-        min_access = minimum_value_accessible_neighbors(arena_map, base_pos);
+        wall_array = detect_wall();
+        min_access = minimum_value_accessible_neighbors(arena_map, poped, wall_array); //returns index of minimum value accessible neighbor
+
+        if (min_access == -1){ //if all neighbors have higher cost than current node
+
+            arena_map[poped[0]][poped[1]] += 1;
+
+            for (int i = 0; i<4; i++){ //pushing accessible neighbors to queue
+                if (wall_array[i] == 0){
+                    switch (i){
+                        case (0):
+                            queue_push(poped[0], poped[1] - 1);
+                            break;
+
+                        case (1):
+                            queue_push(poped[0] - 1, poped[1]);
+                            break;
+
+                        case (2):
+                            queue_push(poped[0], poped[1] + 1);
+                            break;
+
+                        case (3):
+                            queue_push(poped[0] + 1, poped[1]);
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
     }
-    // for (int i = 0; i<4; i++){
-    //     if (wall_array[i] == 0){
-    //         switch(i){
-    //             case 0:
-    //                 queue_push(base_pos[0], base_pos[1] -1);
-    //                 break;
-    //             case 1:
-    //                 queue_push(base_pos[0]-1, base_pos[1]);
-    //                 break;
-    //             case 2: 
-    //                 queue_push(base_pos[0], base_pos[1] -1);
-    //                 break;
-    //             case 3: 
-    //                 queue_push(base_pos[0]+1, base_pos[1]);
-    //                 break;
-    //         }
-    //     }
-    // }
 }
 
 
 void final_action(int **arena_map, int bot_pos[2]){
+    // Main function that decided the action to be taken. Function unverified
     int *smallest_value;
-    int min_access = minimum_value_accessible_neighbors(arena_map, bot_pos);
+    int *wall_array = detect_wall();
+    int min_access = minimum_value_accessible_neighbors(arena_map, bot_pos, wall_array);
 
     switch (min_access){
         case 0://move left
@@ -96,16 +124,26 @@ void final_action(int **arena_map, int bot_pos[2]){
     }
 }
 
-int minimum_value_accessible_neighbors(int ** arena_map, int pos[2]){
-    int *wall_array = detect_wall();
+int minimum_value_accessible_neighbors(int ** arena_map, int *pos, int *wall_array){
+    /*returns 0 for left, 1 for forward, 2 for right, 3 for back, -1 if no minimum accessible neighbors
+    Function unverified
+    */
+
     int *min_cost = minimum_cost(arena_map, pos);
+
     for (int i =0; i< 4; i++){
-        if (arena_map[pos[0]][pos[1]]>min_cost[i]){
-            //check for obstacles in path, if obstacle continue;
-            return i;
+
+        if (arena_map[pos[0]][pos[1]]>min_cost[i]){ //Checking if current node is greater than minimum accessible neighbors.
+            if (wall_array[min_cost[i]] == 0){ //Checking if node is accessible
+                return i;
+            }   
+            else{
+                continue;
+            }
         }
         else{
             return -1;
         }
+
     }
 }
