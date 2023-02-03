7178bd0c8d4c4898e8a862c84b481d45df13351b
Jason Shaun DMello <jason.dmello21@gmail.com>
2023-02-03 21:12:59 +0530

    minimum_cost now returns array, updated other functions, worked furthur with flood map main code

--- bot_functions.c
+++ bot_functions.c
@@ -4,9 +4,15 @@
 int queue_size;
 int queue[]; //queue for storing the path
 
-int* minimum_cost(int **arena_map, int bot_pos[2], int *smallest_value){
+void swap(int *x, int *y){
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int* minimum_cost(int **arena_map, int bot_pos[2]){
     /*
-        returns array with 1 if the direction is the smallest cost in [l, s, r, b] format
+        returns arnray with [0,1,2,3] as [l,s,r,b] in ascending order
         to do: check for array out of bound
     */
 
@@ -15,37 +21,23 @@ int* minimum_cost(int **arena_map, int bot_pos[2], int *smallest_value){
     int left   = arena_map[bot_pos[0]][bot_pos[1] - 1];
     int right  = arena_map[bot_pos[0]][bot_pos[1] + 1];
     int *return_value = calloc (4, sizeof(int));
-
-    if ((left <= top) && (left <= bottom) && (left <= right)){
-        *smallest_value = left;
-        return_value[0] = 1;
-    }
-    else{
-        return_value[0] = 0;
+    int temp_arr[4] = {left, top, right, bottom};
+    int smallest =0, temp = 0;
+    
+    for (int i = 0; i<4; i++){
+        return_value[i] =i;
     }
 
-    if ((top <= bottom) && (top <= left) && (top <= right)){
-        *smallest_value = top;
-        return_value[1] = 1;
-    }
-    else{
-        return_value[1] = 0;
-    }
-
-    if ((right <= top) && (right <= bottom) && (right <= left)){
-        *smallest_value = right;
-        return_value[2] = 1;
-    }
-    else{
-        return_value[2] = 0;
-    }
-
-    if ((bottom <= top) && (bottom <= left) && (bottom <= right)){
-        *smallest_value = bottom;
-        return_value[3] = 1;
-    }
-    else{
-        return_value[3] = 0;
+    //Sorting array
+    for (int i  =0 ; i<4; i++){
+        smallest = i;
+        for (int j = i +1;j<4; j++){
+            if (temp_arr[i]>temp_arr[j]){
+                smallest = j;
+            }
+        }
+        swap(temp_arr[i], temp_arr[smallest]);
+        swap(return_value[i], return_value[smallest]);
     }
 
     return return_value;
@@ -56,69 +48,64 @@ int* detect_wall(){
     returns an array [l, s, r, b] with 1 if wall is detected and 0 if not*/
 }
 
-void check_surroundings(int **arena_map, int curr_row, int curr_col, int* wall_array, int smallest_value){
-    int row = curr_row;
-    int col = curr_col;
-    int bot_pos[2] = {row, col};
-    
-    int smallest_value;
-    int min_cost;
-    
-    add_queue(&queue, queue_size, row, col);
-
-    while (!queue_empty(queue, queue_size)){
-        remove_queue(queue, queue_size, &row, &col);
-
-        min_cost = minimum_cost(arena_map, bot_pos, &smallest_value);
-
-        if (arena_map[row][col] <= smallest_value){
-            arena_map[row][col] += 1;
-            for (int i = 0; i<4; i++){
-                if (wall_array[i] == 0){
-                    add_queue(&queue, queue_size, row, col);
-                }
-            }
-        }   
-
+void rearrange_map(int ** arena_map, int base_pos[2]){
+    /*Not Complete*/
+    queue_push(base_pos[0], base_pos[1]);
+    int *poped;
+    int min_access;
+    while (!queue_empty()){
+        poped = queue_pop();
+        min_access = minimum_value_accessible_neighbors(arena_map, base_pos);
     }
+    // for (int i = 0; i<4; i++){
+    //     if (wall_array[i] == 0){
+    //         switch(i){
+    //             case 0:
+    //                 queue_push(base_pos[0], base_pos[1] -1);
+    //                 break;
+    //             case 1:
+    //                 queue_push(base_pos[0]-1, base_pos[1]);
+    //                 break;
+    //             case 2: 
+    //                 queue_push(base_pos[0], base_pos[1] -1);
+    //                 break;
+    //             case 3: 
+    //                 queue_push(base_pos[0]+1, base_pos[1]);
+    //                 break;
+    //         }
+    //     }
+    // }
 }
 
 
 void final_action(int **arena_map, int bot_pos[2]){
+    int *smallest_value;
+    int min_access = minimum_value_accessible_neighbors(arena_map, bot_pos);
+
+    switch (min_access){
+        case 0://move left
+            break;
+        case 1: //move forward
+            break;
+        case 2: //move right
+            break;
+        case 3: // turn around
+            break;
+        case -1:
+            rearrange_map(arena_map, bot_pos);
+    }
+}
+
+int minimum_value_accessible_neighbors(int ** arena_map, int pos[2]){
     int *wall_array = detect_wall();
-    int smallest_value;
-    int min_cost = minimum_cost(arena_map, bot_pos, &smallest_value);
-
-    if (min_cost == 0){
-        if (wall_array[0] == 1){
-
+    int *min_cost = minimum_cost(arena_map, pos);
+    for (int i =0; i< 4; i++){
+        if (arena_map[pos[0]][pos[1]]>min_cost[i]){
+            //check for obstacles in path, if obstacle continue;
+            return i;
         }
         else{
-            left();
-        }
-    }
-    else if (min_cost == 1){
-        if (wall_array[1] == 1){
-
-        }
-        else{
-            forward();
-        }
-    }
-    if (min_cost == 2){
-        if (wall_array[2] == 1){
-
-        }
-        else{
-            right();
-        }
-    }
-    if (min_cost == 3){
-        if (wall_array[3] == 1){
-
-        }
-        else{
-            turn_around();
+            return -1;
         }
     }
 }
