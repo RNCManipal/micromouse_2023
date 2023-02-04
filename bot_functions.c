a12e00851e128ffecb19523f678cd9776acfa662
Jason Shaun DMello <jason.dmello21@gmail.com>
2023-02-04 19:21:01 +0530

    Added comments for easier understanding + fixed bugs + started work on queues(not done)

--- bot_functions.c
+++ bot_functions.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*This project uses flood fill algorithm Pseudocode is as follows - 
+/*This project uses flood fill algorithm. Pseudocode is as follows - 
     1. Move mouse to lowest cost node
     2. If all neighbors are higher than or equal to current node, push node to queue.
     3. increment current node by 1 and push acessible neighbors to queue
@@ -19,19 +19,44 @@ void swap(int *x, int *y){
 int* minimum_cost(int **arena_map, int bot_pos[2]){
     /*
         returns array with [0,1,2,3] as [l,s,r,b] in ascending order
-        Function unverified
-        to do: check for array out of bound
+        Function 90% verified
     */
 
    //Getting values of neighbors
-    int top    = arena_map[bot_pos[0] - 1][bot_pos[1]];
-    int bottom = arena_map[bot_pos[0] + 1][bot_pos[1]];
-    int left   = arena_map[bot_pos[0]][bot_pos[1] - 1];
-    int right  = arena_map[bot_pos[0]][bot_pos[1] + 1];
+   int top, left, bottom, right;
+   
+   //if condition is to check for array out of bound condition
+    if (bot_pos[0] == 0){ //if bot is at top row
+        top = 1000;
+    }
+    else{
+        top = arena_map[bot_pos[0] - 1][bot_pos[1]];
+    }
+
+    if (bot_pos[0] == 15){ //if bot is at bottom row
+        bottom = 1000;
+    }
+    else{
+        bottom = arena_map[bot_pos[0] + 1][bot_pos[1]];
+    }
+
+    if (bot_pos[1] == 0){ //if bot is at leftmost column
+        left = 1000;
+    }
+    else{
+        left = arena_map[bot_pos[0]][bot_pos[1] - 1];
+    }
+
+    if (bot_pos[1] == 15){ //if bot is at rightmost column
+        right = 1000;
+    }
+    else{
+        right = arena_map[bot_pos[0]][bot_pos[1] + 1];
+    }
 
     int *return_value = calloc (4, sizeof(int)); //array to be returned
     int temp_arr[4] = {left, top, right, bottom}; //array to be sorted
-    int smallest =0, temp = 0;
+    int smallest =0;
     
     for (int i = 0; i<4; i++){
         return_value[i] =i; //initializing return array to [0,1,2,3]
@@ -44,11 +69,16 @@ int* minimum_cost(int **arena_map, int bot_pos[2]){
             if (temp_arr[i]>temp_arr[j]){
                 smallest = j;
             }
+            else if (temp_arr[i] == temp_arr[j]){ //if two values are equal, l, s, r, b order is maintained
+                if (return_value[i] > return_value[j]){
+                    swap(&return_value[i], &return_value[j]);
+                }
+            }
         }
-        swap(temp_arr[i], temp_arr[smallest]);
-        swap(return_value[i], return_value[smallest]);
-    }
 
+        swap(&temp_arr[i], &temp_arr[smallest]);
+        swap(&return_value[i], &return_value[smallest]);
+    }
     return return_value;
 }
 
@@ -65,15 +95,16 @@ void rearrange_map(int ** arena_map, int base_pos[2]){
     int *poped;
     int min_access;
     int *wall_array;
+    int small;
 
     while (!queue_empty()){
         poped = queue_pop();
         wall_array = detect_wall();
-        min_access = minimum_value_accessible_neighbors(arena_map, poped, wall_array); //returns index of minimum value accessible neighbor
+        min_access = minimum_value_accessible_neighbors(arena_map, poped, wall_array, small); //returns index of minimum value accessible neighbor
 
-        if (min_access == -1){ //if all neighbors have higher cost than current node
+        if (min_access == -1){ //if all accessible neighbors have higher cost than current node
 
-            arena_map[poped[0]][poped[1]] += 1;
+            arena_map[poped[0]][poped[1]] = small + 1;
 
             for (int i = 0; i<4; i++){ //pushing accessible neighbors to queue
                 if (wall_array[i] == 0){
@@ -103,28 +134,7 @@ void rearrange_map(int ** arena_map, int base_pos[2]){
     }
 }
 
-
-void final_action(int **arena_map, int bot_pos[2]){
-    // Main function that decided the action to be taken. Function unverified
-    int *smallest_value;
-    int *wall_array = detect_wall();
-    int min_access = minimum_value_accessible_neighbors(arena_map, bot_pos, wall_array);
-
-    switch (min_access){
-        case 0://move left
-            break;
-        case 1: //move forward
-            break;
-        case 2: //move right
-            break;
-        case 3: // turn around
-            break;
-        case -1:
-            rearrange_map(arena_map, bot_pos);
-    }
-}
-
-int minimum_value_accessible_neighbors(int ** arena_map, int *pos, int *wall_array){
+int minimum_value_accessible_neighbors(int ** arena_map, int *pos, int *wall_array, int *smallest_accessible_regardless){
     /*returns 0 for left, 1 for forward, 2 for right, 3 for back, -1 if no minimum accessible neighbors
     Function unverified
     */
@@ -141,9 +151,56 @@ int minimum_value_accessible_neighbors(int ** arena_map, int *pos, int *wall_arr
                 continue;
             }
         }
-        else{
-            return -1;
-        }
 
+        else{
+            if (wall_array[min_cost[i]] == 0){ //Checking if node is accessible
+                switch(i){ //assigning smallest_accessible_regardless to the smallest non-accessible neighbor
+                    case 0:
+                        *smallest_accessible_regardless = arena_map[pos[0]][pos[1] - 1];
+                        break;
+                    case 1:
+                        *smallest_accessible_regardless = arena_map[pos[0] - 1][pos[1]];
+                        break;
+                    case 2:
+                        *smallest_accessible_regardless = arena_map[pos[0]][pos[1] + 1];
+                        break;
+                    case 3:
+                        *smallest_accessible_regardless = arena_map[pos[0] + 1][pos[1]];
+                        break;
+                    default:
+                        break;
+                }
+                return -1;
+            }
+        }
     }
+}
+
+int final_action(int **arena_map, int bot_pos[2]){
+    // Main function that decided the action to be taken. Function unverified
+
+    int *smallest_value;
+    int *wall_array = detect_wall();
+    int small;
+    int min_access;
+
+    do{
+        min_access = minimum_value_accessible_neighbors(arena_map, bot_pos, wall_array, &small);
+        switch (min_access){
+            case 0://move left
+                return 0;
+                break;
+            case 1: //move forward
+                return 1;
+                break;
+            case 2: //move right
+                return 2;
+                break;
+            case 3: // turn around
+                return 3;
+                break;
+            case -1:
+                rearrange_map(arena_map, bot_pos);
+        }
+    }while (min_access != -1);
 }
--- data_structures.c
+++ data_structures.c
@@ -1,34 +1,92 @@
 #include<stdio.h>
 #include <stdlib.h>
+#define SIZE 50
 
-/*Not done at all, needs to be attempted again from scratch with a circular queue*/
+/*Still working on it, almost done*/
 
+int ** queue;
+int first, last;
 
-typedef struct queue{
-    int row;
-    int col;
-    int data;
-};
-
-
-int first = -1;
-int last = -1;
-
-void add_queue(int *queue, int size, int value){
-    if (last == size -1){
-        printf("Unsuccessful queue allocation");
-    }
-    else{
-        queue[++last] = value;
+int queue_empty(){
+    if (first == last){
         return 1;
+    }
+    return 0;
+}
+
+int queue_full(){
+    if ((first +1)%SIZE == last){
+        return 1;
+    }
+    return 0;
+}
+
+void queue_push (int x, int y){
+    if (queue_full()){
+        return;
+    }
+
+    last = (last+1)%SIZE;
+    queue[last] = calloc (2, sizeof(int));
+    queue[last][0] = x;
+    queue[last][1] = y;
+    printf("Done");
+}
+
+int* queue_pop (){
+
+    if (queue_empty()){
+        return NULL;
+    }
+
+    first = (first + 1)%SIZE;
+    int * temp = queue[first];
+    free (queue[first]);
+    return temp;
+}
+
+void display(){
+    for (int i = first; (i<SIZE)||(i <= last); i++){
+        printf (" i =%d %d ", queue[i][0], queue[i][1]);
+    }
+
+    if (first > last){
+        for (int i = 0; i <= last; i++){
+            printf (" i =%d %d ", queue[i][0], queue[i][1]);
+        }
     }
 }
 
-int remove_queue(int *queue, int size){
-    if (first == last){
-        return -1;
+int main(){
+    queue = calloc (SIZE, sizeof(int*));
+    first =0;
+    last = 0;
+    int ch=0;
+    int x, y;
+    int *pop = calloc (2, sizeof(int));
+
+    while (ch!=3){
+        printf("\n Push pop display \n");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+        case 0:
+            scanf("%d %d", &x, &y);
+            queue_push(x,y);
+            break;
+        
+        case 1:
+            pop = queue_pop();
+            break;
+
+        case 2:
+            display();
+            break;
+
+        default:
+            break;
+        }
     }
-    else{
-        return queue[++first];
-    }
+
+    return 0;
 }
