41034509df371e56a2ae122816e899c700238b6c
adi673 <codingadityawadgaonkar@gmail.com>
2023-05-10 16:39:53 +0530

    pid.c code tested left with kp kd values

--- main/pid.c
+++ main/pid.c
@@ -2,41 +2,90 @@
 #include <stdlib.h>
 #include "bot_functions.h"
 
-void p2p_pid(int dist) //Point to  Point PID    
-{   
-    // PID Controller. Used to control the speed of the bot. Function unverified
-    double rotatn_req = (dist / WHEEL_DIAMETER); // number of rotations of wheel required to complete given distance
-    // let encoder giving 'x' number of counts per rotation
+void p2p_pid(int dist) {
+  // PID Controller. Used to control the speed of the bot. Function
+  double rotatn_req = (dist /(3.14*WHEEL_DIAMETER)); // number of rotations ofwheel required to complete given distance
 
-    double setpnt_counts = (rotatn_req) * (counts_per_rotation); // number of counts required to reach the set point(inshort this is our setpoint)
-    count = 0;
-    double lasterror = 0, error = 0;
-    double pv = 0;
+  // let encoder giving 'x' number of counts per rotation
+  double setpnt_counts =(rotatn_req) * (520); // number of counts requiredto reach the set point(inshort this is our setpoint)
 
-    while (1)
-    {
-        error = (setpnt_counts) - (count); // x is the number of encoder counts per revolution
+  count=0;
+  double lasterror = 0;
+  double kp1 = 0.1;
+  double kd1 = 1.6;
+  double last_error = 0, error = 0;
+  double pv = 0;
 
-        if (lasterror == 0)
-        { // this condition is used to remove intial high gain in velocity
-            pv = kp1 * error;
-        }
-        else
-        {
-            pv = kp1 * error + kd1 * (error - lasterror);
-        }
+  // double maxerror = kp1*setpnt_counts + kd1*(setpnt_counts);
 
-        lasterror = error;
+  if (setpnt_counts < 0) {
+    while (1) {
 
-        if (pv >= -0.1 && pv <= 0.1)
-        { // Assuming lower and upper thresholds of speed of motors are 1, 200 respectively
-            brake();
-            break;
-        }
+      error = (count) - (setpnt_counts);
+     
+      if (lasterror ==0) { // this condition is used to remove intial high gain in velocity
+        pv = kp1 * error;
+      } else {
+        pv = kp1 * error + kd1 * (error - lasterror);
+      }
 
-        int speed = min(max(pv, -200), 200);
-        Motor_SetSpeed(speed, speed); // Set the parameters later. these parameters are speed of left and right wheel and will be for forward motion since pv>=0
+
+      // pv= mapp(pv, -maxerror, maxerror, -255, 255);
+      if (pv >= -0.3 && pv <= 0.3) { // Assuming lower and upper thresholds of speed of motors are 50, 200 respectively
+         brake();
+      
+
+        break;
+      } else if (pv > 0) {
+        int speed = min(max(pv, 50), 200);
+        Motor_SetSpeed(-speed, -speed); //Set the parameters later.
+        // theseparameters are speed of left and right wheel and will be
+        // forforward motion since pv>=0
+      
+      } else {
+        int speed = min(max(pv, -200), -50);
+        
+        Motor_SetSpeed(speed, speed); //Set the parameters later.
+        // theseparameters are speed of left and right wheel and will be
+        // forforward motion since pv>=0
+
+      }
     }
+  } else if (setpnt_counts > 0) {
+    while (1) {
+
+      error = (setpnt_counts) -(count); // x is the number of encoder countsper revolution
+      
+
+      if (lasterror ==0) { // this condition is used to remove intial high gain in velocity
+        pv = kp1 * error;
+      } else {
+        pv = kp1 * error + kd1 * (error - lasterror);
+      }
+
+      lasterror = error;
+
+      if (pv >= -0.3 && pv <= 0.3) { // Assuming lower and upper thresholds of speed ofmotors are 50, 200 respectively
+        brake();
+        break;
+      } else if (pv > 0) {
+        int speed = min(max(pv, 50), 200);
+       
+        Motor_SetSpeed(speed, speed); //Set the parameters later.
+        // theseparameters are speed of left and right wheel and will be
+        // forforward motion since pv>=0
+        
+
+      } else if (pv < 0) {
+        int speed = min(max(pv, -200), -50);
+        
+        Motor_SetSpeed(speed, speed); //Set the parameters later.
+        // theseparameters are speed of left and right wheel and will be
+        // forforward motion since pv>=0
+
+      }
+    }
+  }
 }
 
 void sens_pid()
